9ed82410898bd06a52e1e0f64190e015de491fc9
eric van tassell <Eric.VanTassell@amd.com>
2018-11-20 00:53:30 -0600

    new files

---
+++ notebook_clone.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+#
+
+# debug stuff
+import pdb
+
+# support code
+import tsc_util as tscu
+import tsc_datadict as dd
+
+#step 0
+# Load pickled data
+import pickle
+    
+# TODO: Fill this in based on where you saved the training and testing data
+dd = dd.DataDict().get_dict()
+
+### Replace each question mark with the appropriate value. 
+### Use python, pandas or numpy methods rather than hard coding the results
+import numpy as np
+
+# TODO: Number of training examples
+n_train = dd['train']['X'].shape[0]
+
+# TODO: Number of validation examples
+n_validation = dd['valid']['X'].shape[0]
+
+# TODO: Number of testing examples.
+n_test = dd['test']['X'].shape[0]
+
+# 2DO: What's the shape of an traffic sign image?
+image_shape = "(" + str(dd['train']['X'].shape[1]) + "," + str(dd['train']['X'].shape[2]) + ")"
+
+# 2DO: How many unique classes/labels there are in the dataset.
+# now part of dd
+
+print("shape(features) = " + str(dd['train']['X'].shape ))
+print("shape(labels) = " + str(dd['train']['y'].shape ))
+print("Number of training examples =", n_train)
+print("Number of validation examples =", n_validation)
+print("Number of testing examples =", n_test)
+print("Image data shape =", image_shape)
+print("Number of classes =", dd['n_classes'])
+
+sn_dict = tscu.get_id2name_dict()
+assert("FIXME:change the signature" == None)
+
+signs_by_set_by_id = tscu.organize_signs_by_id([(dd['train']['y'], 'train'), (dd['valid']['y'], 'valid'),
+                                              (dd['test']['y'], 'test')], n_classes)
+
+assert("FIXME:change the signature" == None)
+
+sample_signs = get_sample_signs(dd['train']['X'], dd['train']['y'], n_classes, sn_dict,
+                                signs_by_set_by_id['train'])
+
+pdb.set_trace()
---
+++ tsc_datadict.py
@@ -0,0 +1,26 @@
+import pickle
+import pdb
+import numpy as np
+import tsc_util as tscu
+
+class DataDict:
+    def get_set_names(self):
+        return [ 'train', 'valid', 'test']
+
+    def set_n_classes(self):
+        set_names = self.get_set_names()
+        all_labels = self.dd[set_names[0]]['y']
+        for set_name in set_names[1:]:
+            np.append(all_labels, self.dd[set_name]['y'])
+            self.dd['n_classes'] = len(np.unique(all_labels))
+        
+    def get_dict(self):
+        return self.dd
+
+    def __init__(self):
+        self.dd = dict()
+        for set_name in self.get_set_names():
+            print("\t set_name = " + set_name)
+            pd = tscu.get_pickle_dict(set_name)
+            self.dd[set_name] = { 'X' : pd['features'], 'y' : pd['labels']}
+        self.set_n_classes()
